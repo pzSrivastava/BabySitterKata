3a5298573709a3d326dcaabbdaee6c5827fa591d
Prashant Srivastava <p.z.srivastava@accenture.com>
2021-08-10 19:46:00 +0530

    Family A Charges Done with all Test Case

---
+++ BabySitterKata/BabySitterCalculatCharge.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace BabySitterKata
+{
+    public class BabySitterCalculatCharge
+    {
+        int famalyAWage = 15;
+        int famalyASpecialWageAfter11 = 20;
+        int babySitterStartTime = 17;
+
+        const int RegularTimeStart = 1;
+        const int RegularTimeEnd = 12;
+        const int ExtraMoneyTime = 23;
+        const int midNightTime = 24;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public double CalculateBabySitterRegularHoursPay(int startTime, int endTime)
+        {
+            double cost = 0;
+            if (startTime < babySitterStartTime)
+                startTime = babySitterStartTime;
+
+            if (startTime >= babySitterStartTime && endTime <= ExtraMoneyTime)
+                cost = (endTime - startTime) * famalyAWage;
+
+            return cost;
+
+        }
+        public double CalculateBabySitterPremiumHoursPay(int startTime, int endTime)
+        {
+            double cost = 0;
+
+            int startToBefore11 = (ExtraMoneyTime - startTime) * famalyAWage;
+            int actualEndTime = endTime >= RegularTimeStart && endTime <= RegularTimeEnd ? (midNightTime + endTime) : endTime;
+            int after11 = actualEndTime < ExtraMoneyTime ? 0 : (actualEndTime - ExtraMoneyTime) * famalyASpecialWageAfter11;
+            cost = startToBefore11 + after11;
+            return cost;
+
+        }
+
+
+        //public double CalculateBabySitterPremiumHours(int startTime, int endTime)
+        //{
+        //    double cost = 0;
+        //    // 5PM -> 2AM
+        //    // 5 PM - 11 PM = 6 hr * 15
+        //    // 11 PM - 2 AM = 3hr * 20   ==> 150           
+
+        //    int startToBefore11 = (boudarytime - startTime) * famalyAWage;
+        //    int after11 = endTime < boudarytime ? 0 : (endTime - boudarytime) * famalyASpecialWageAfter11;
+        //    cost = startToBefore11 + after11;
+        //    return cost;
+
+        //}
+    }
+}
---
+++ BabySitterKataTest/UnitTest1.cs
@@ -0,0 +1,62 @@
+using BabySitterKata;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using Xunit;
+
+namespace BabySitterKataTest
+{
+    public class BabySitterTest
+    {
+        
+        [Fact]
+        public void TestCalculateChargeforFamilyAatRegulerOneHours()
+        {
+            int startTime,endTime;
+            startTime = 17;      // 5 PM
+            endTime = 18;        // 6 pM     
+
+            Assert.Equal(15, new BabySitterCalculatCharge().CalculateBabySitterRegularHoursPay(startTime, endTime));
+        }
+
+        [Fact]
+        public void TestCalculateChargeforFamilyAatRegulertwoHours()
+        {
+            int startTime, endTime;
+            startTime = 17;      // 5 PM
+            endTime = 19;        // 7 pM     
+
+            Assert.Equal(30, new BabySitterCalculatCharge().CalculateBabySitterRegularHoursPay(startTime, endTime));
+        }
+
+        [Fact]
+        public void TestCalculateChargeforFamilyAatIrRegulerHours()
+        {
+            int startTime, endTime;
+            startTime = 16;      // 4 PM  -- baby sitter reaches before 5pm
+            endTime = 19;        // 7 pM     
+
+            Assert.Equal(30, new BabySitterCalculatCharge().CalculateBabySitterRegularHoursPay(startTime, endTime));
+        }
+
+        [Fact]
+        public void TestCalculateChargeforFamilyAfter11pm()
+        {
+            int startTime, endTime;
+            startTime = 17;      //  5PM - 11 pm = 6hr
+            endTime = 24;        // 11 -12 pm     
+
+            Assert.Equal(110, new BabySitterCalculatCharge().CalculateBabySitterPremiumHoursPay(startTime, endTime));
+        }
+
+        [Fact]
+        public void TestCalculateChargeforFamilyTill2Am()
+        {
+            int startTime, endTime;
+            startTime = 17;      //  5PM - 11 pm = 6hr * 15  // ||  17 - 23 = 6 * 15 
+            endTime = 2;     //14  // 2AM  11 - 2 AM = 3 *20    || 23 - [24 + 2 ]=  3 * 20
+
+            Assert.Equal(150, new BabySitterCalculatCharge().CalculateBabySitterPremiumHoursPay(startTime, endTime));
+        }
+
+    }
+}
